c7eb1d489b2d8b625ce5a17d4887f3c10a80a152
muWork <murilo.work01@gmail.com>
2025-10-16 06:36:53 -0300

    ajuste run_server.py

--- src/run_server.py
+++ src/run_server.py
@@ -1,12 +1,10 @@
 # Este script é para ser executado em um console Bash no PythonAnywhere.
 # Ele inicia o servidor NiceGUI real em uma porta interna.
 
-from nicegui import ui # Importa o ui diretamente para o teste
+from main import ui
 
 # A porta 8000 é um exemplo, pode ser outra porta alta.
-PORT = 8001
-
-ui.label("Teste de Conexão Bem-Sucedido!") # Adiciona um elemento simples
+PORT = 8000
 
 print(f"Iniciando servidor NiceGUI na porta {PORT}")
-ui.run(host='127.0.0.1', port=PORT)
+ui.run(host='0.0.0.0', port=PORT)
