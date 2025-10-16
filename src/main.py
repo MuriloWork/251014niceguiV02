23ffdd65ae782599b17615b6beec31a781088683
muWork <murilo.work01@gmail.com>
2025-10-16 08:34:46 -0300

    ajuste main.py

--- src/main.py
+++ src/main.py
@@ -1523,3 +1523,9 @@ app.on_startup(inicializar_estado)
 
 # Save the application's general state when the server shuts down.
 app.on_shutdown(salvar_estado_no_db)
+
+# A guarda `if __name__ in {"__main__", "__mp_main__"}` garante que `ui.run()`
+# seja chamado apenas quando o script é executado diretamente.
+# Isso é essencial para o desenvolvimento local e não interfere na implantação com Uvicorn.
+if __name__ in {"__main__", "__mp_main__"}:
+    ui.run(host=HOST, port=8080, reload=True, show=True, title='muWorkApp')
